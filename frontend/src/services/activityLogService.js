7a29a4b34b360485fc3f3c0249695a57f6a6e6af
Arif Rahman Hakim <162030147+Arif04218@users.noreply.github.com>
2026-06-19 05:16:01 +0700

    Delete frontend directory

--- frontend/src/services/activityLogService.js
+++
@@ -1,4 +0,0 @@
-import api from "./api";
-
-export const getMyActivityLogs = () => api.get("/user/activity-logs");
-export const getAdminActivityLogs = () => api.get("/admin/activity-logs");
--- frontend/src/services/visitService.js
+++
@@ -1,4 +0,0 @@
-import api from "./api";
-
-export const logArticleVisit = (id) => api.post(`/user/articles/${id}/visit`);
-export const logRecipeVisit = (id) => api.post(`/user/recipes/${id}/visit`);
